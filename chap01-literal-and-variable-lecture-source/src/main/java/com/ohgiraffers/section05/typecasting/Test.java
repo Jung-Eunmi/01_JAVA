0fddd75937d97832d87b1fed041640cb49723ce2
Jung-Eunmi <dmssl2486@gmail.com>
2024-08-27 16:11:10 +0900

    [24.08.27 chap02 start]

--- chap01-literal-and-variable-lecture-source/src/main/java/com/ohgiraffers/section05/typecasting/Test.java
+++ chap01-literal-and-variable-lecture-source/src/main/java/com/ohgiraffers/section05/typecasting/Test.java
@@ -7,10 +7,28 @@ public class Test {
         double b = 50.6;
         double c = 70.8;
 
-        int sum = (int)(a + b + c);
-        System.out.println(" 총점 : " + sum);
-        int average = (int)((a + b + c)/3);
-        System.out.println(" 평균 : " + average);
+        double sum1 = a + b + c;
+        System.out.println("sum = " + sum1);
+        double average1 = sum1 / 3;
+        System.out.println("average1 = " + average1);
+
+        int sum2 = (int)(a + b + c);
+        System.out.println(" 총점 : " + sum2);
+        int average2 = (sum2/3);
+        System.out.println(" 평균 : " + average2);
+        
+        long sum3 = (long)(a + b + c);
+        System.out.println("sum3 = " + sum3);
+        long average3 = (sum3/3);
+        System.out.println("average3 = " + average3);
+
+        int elevator = 1000;
+        int people = 76;
+
+        int a1 = elevator/people;
+        System.out.println(" 엘레베이터 수용 인원 = " + a1 + "명");
+        double b1 = (double) elevator/13;
+        System.out.println(" 엘레베이터에 탈 수 있는 13명 평균 몸무게 = " + b1 + "kg");
 
     }
 }
---
+++ chap02-operator-lecture-source/src/main/java/com/ohgiraffers/section02/assginment/Application.java
@@ -0,0 +1,29 @@
+package com.ohgiraffers.section02.assginment;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        /* title. 대입연산자와 산술 복합 대입 연산자를 이해할 수 있다. */
+        
+        /* comment.
+        *   '=' : 왼쪽의 피연산자에 오른쪽의 피연산자를 대입함
+        *   '+=' : 왼쪽의 피연산자에 오른쪽의 피연산자를 더한 결과를 왼쪽의 피연산자에 대입함
+        *   '-='
+        *   '*='
+        *   '/='
+        *   '%='
+        *   */
+
+        // ex. num =- 5; '-='를 '=-'로 쓴다면?
+        // 주의사항! 작성 순서가 바뀌게 되면 -5 숫자를 num 공간에 대입된다.
+
+
+
+
+
+
+
+
+    }
+}

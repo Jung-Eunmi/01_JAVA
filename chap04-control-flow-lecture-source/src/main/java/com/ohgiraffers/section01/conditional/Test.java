f8b53bce1f0dd10c37260d33201bf2d15451b5f4
Jung-Eunmi <dmssl2486@gmail.com>
2024-09-02 13:18:07 +0900

    [24.09.02]

---
+++ chap04-control-flow-lecture-source/src/main/java/com/ohgiraffers/section01/conditional/Test.java
@@ -0,0 +1,31 @@
+package com.ohgiraffers.section01.conditional;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Test {
+
+    public void diet(){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("몸무게를 입력하세요 : ");
+        int num = sc.nextInt();
+        Random random = new Random();
+        int chicken = random.nextInt(2);
+
+        if(num>70){
+            System.out.println("샐러드를 먹어야겠다.");
+            if(chicken>0) {
+                System.out.println("ggh");
+
+            }
+        } else {
+            System.out.println("라면을 먹어야겠다.");
+        }
+
+
+
+    }
+
+
+}

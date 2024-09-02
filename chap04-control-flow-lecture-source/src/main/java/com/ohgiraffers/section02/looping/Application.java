8a28181ce47c28b6da627a49641c1414d75245ac
Jung-Eunmi <dmssl2486@gmail.com>
2024-09-02 15:37:11 +0900

    [24.09.2]수수정

---
+++ chap04-control-flow-lecture-source/src/main/java/com/ohgiraffers/section02/looping/Application.java
@@ -0,0 +1,19 @@
+package com.ohgiraffers.section02.looping;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+//        A_for a = new A_for();
+//        a.testSimpleForStatement();
+        A_for b = new A_for();
+        b.testForExample1();
+
+
+
+
+
+
+    }
+
+}

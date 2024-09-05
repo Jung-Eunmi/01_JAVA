5d2d530946a6fa816aa280450cd55505ab3c2315
Jung-Eunmi <dmssl2486@gmail.com>
2024-09-05 11:43:17 +0900

    [240905]

---
+++ chap05-array-lecture-source/src/main/java/com/ohgiraffers/section02/copy/Application3.java
@@ -0,0 +1,31 @@
+package com.ohgiraffers.section02.copy;
+
+public class Application3 {
+
+    public static void main(String[] args) {
+
+        /* title. 깊은 복사에 대해 이해할 수 있다. */
+
+        /* comment.
+        *   깊은 복사란?
+        *   heap 에 생성된 배열이 가지고 있는 값을 또 다른 배열에 복사를 해 둔 것이다.
+        *   서로 같은 값을 가지고 있지만, 두 배열은 서로 다른 배열이기 때문에
+        *   원본 또는 복제본을 수정하더라도 서로 영향을 미치지 않는다.
+        * */
+
+        /* comment.
+        *   깊은 복사를 하는 방법은 4가지가 있다.
+        *   1. for 문을 이용한 동일한 인덱스의 값 복사
+        *   2. Object 의 clone() 메소드를 이용한 값 복사
+        *   3. System 의 arraycopy() 메소드를 이용한 값 복사
+        *   4. Array 의 copyOf() 메소드를 이용한 값 복사
+        * */
+
+        
+
+
+
+
+    }
+
+}

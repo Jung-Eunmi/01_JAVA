c911cfac341a11af642d84db23ff23432c60b583
Jung-Eunmi <dmssl2486@gmail.com>
2024-09-09 14:33:23 +0900

    [240909]수수수정

---
+++ chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section05/parameter/ParameterTest.java
@@ -0,0 +1,16 @@
+package com.ohgiraffers.section05.parameter;
+
+public class ParameterTest {
+
+    public void testPrimaryParameter(int num) {
+        System.out.println("매개변수로 전달 받은 값 : " + num);
+    }
+
+    
+
+
+
+
+
+
+}

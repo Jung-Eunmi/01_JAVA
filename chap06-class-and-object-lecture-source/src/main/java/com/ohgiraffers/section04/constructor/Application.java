619ee44ce5d884fa951af791110363d7aa1a74ad
Jung-Eunmi <dmssl2486@gmail.com>
2024-09-09 12:14:47 +0900

    [240909]수정

--- chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section04/constructor/Application.java
+++ chap06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section04/constructor/Application.java
@@ -1,5 +1,7 @@
 package com.ohgiraffers.section04.constructor;
 
+import java.time.LocalDateTime;
+
 public class Application {
 
     public static void main(String[] args) {
@@ -14,6 +16,25 @@ public class Application {
         *   매개변수가 없는 생성자는 compiler 에 의해 자동으로 추가되기 때문에
         *   우리가 지금까지 작성하지 않고도 사용할 수 있었다.
         * */
+
+        UserDTO user = new UserDTO();
+        System.out.println("user 의 초기값 : "+user.toString());
+
+        /* comment.
+        *   기본 생성자로 인스턴스를 생성하게 되면 jvm 이 제공하는 기본값들로 값이 초기화가 된다.
+        *   하지만, 우리가 초기값을 원하는 대로 생성하자마자 넣고 싶으면
+        *   매개변수가 있는 생성자로 값을 인자로 전달하면 된다.
+        *   주의점!!!
+        *   만약 매개변수가 있는 생성자가 존재한다면 기본 생성자는 컴파일러가 자동으로 추가해주지 않는다.
+        *   따라서 이 점을 주의해야한다.
+        * */
+
+        UserDTO user2 = new UserDTO("user02","pass02","정은미");
+        System.out.println("user2 의 초기값 : " + user2.toString());
+
+        UserDTO user3 = new UserDTO("user03","pass03","정은미", LocalDateTime.now());
+        System.out.println("user3 의 초기값 : " + user3.toString());
+
 
 
 

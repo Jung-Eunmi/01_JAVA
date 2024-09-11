ab0824d7337b4924bd2d36951b49d8cfd253070e
Jung-Eunmi <dmssl2486@gmail.com>
2024-09-11 10:40:36 +0900

    [240911]

--- chap07-object-array-lecture-source/src/main/java/com/ohgiraffers/section01/Test0910.java
+++ chap07-object-array-lecture-source/src/main/java/com/ohgiraffers/section01/Test0910.java
@@ -5,30 +5,67 @@ import java.util.Scanner;
 public class Test0910 {
 
     public static void main(String[] args) {
-        //키보드로 입력 받은 값들을 변수에 기록하고 저장된 변수 값을 화면에 출력하여 확인하세요.
+        //문제 1 키보드로 입력 받은 값들을 변수에 기록하고 저장된 변수 값을 화면에 출력하여 확인하세요.
         //이 때 성별이 ‘M’이면 남학생, ‘M’이 아니면 여학생으로 출력 처리 하세요.
-        Scanner sc = new Scanner(System.in);
-        System.out.print("이름 : ");
-        String name = sc.nextLine();
-        System.out.print("학년 : ");
-        int grade = sc.nextInt();
-        System.out.print("반 : ");
-        int classNum = sc.nextInt();
-        System.out.print("성별 : ");
-        char mOrF = sc.next().charAt(0);
-        System.out.print("점수 : ");
-        double score = sc.nextDouble();
-        System.out.println();
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("이름 : ");
+//        String name = sc.nextLine();
+//        System.out.print("학년 : ");
+//        int grade = sc.nextInt();
+//        System.out.print("반 : ");
+//        int classNum = sc.nextInt();
+//        System.out.print("성별 : ");
+//        char mOrF = sc.next().charAt(0);
+//        System.out.print("점수 : ");
+//        double score = sc.nextDouble();
+//        System.out.println();
+//
+//        if(mOrF == 'M'){
+//
+//            mOrF = '남';
+//
+//        }else{
+//            mOrF = '여';
+//        }
+//
+//        System.out.println(grade+"학년"+classNum+"반"+name+ " " +  mOrF+"의 성적은"+score+"입니다.");
 
-        if(mOrF == 'M'){
+        //문제2 나이를 키보드로 입력 받아 어린이(13세 이하)인지,
+        // 청소년(13세 초과 ~ 19세 이하)인지,
+        // 성인(19세 초과)인지 출력하세요.
 
-            mOrF = '남';
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("나이를 입력해주세요 : ");
+//        int age = sc.nextInt();
+//
+//        String a = (age>19)?"성인":(age>13 && age<=19)?"청소년":"어린이";
+//        System.out.println(a);
 
-        }else{
-            mOrF = '여';
-        }
+        // 문제3 국어, 영어, 수학에 대한 점수를 키보드를 이용해 정수로 입력 받고,
+        //세 과목에 대한 합계(국어+영어+수학)와 평균(합계/3.0)을 구하세요.
+        //세 과목의 점수와 평균을 가지고 합격 여부를 처리하는데
+        //세 과목 점수가 각각 40점 이상이면서 평균이 60점 이상일 때 합격, 아니라면 불합격을 출력하세요.
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("국어 점수 : ");
+//        int korean = sc.nextInt();
+//        System.out.print("영어 점수 : ");
+//        int english = sc.nextInt();
+//        System.out.print("수학 점수 : ");
+//        int math = sc.nextInt();
+//
+//        int average = ((korean+english+math)*3);
+//
+//        if(average>=60 && korean>=40&& math>=40&& english>=40){
+//            System.out.println("합격");
+//        }else {
+//            System.out.println("불합격");
+//        }
 
-        System.out.println(grade+"학년"+classNum+"반"+name+ " " +  mOrF+"의 성적은"+score+"입니다.");
+
+
+
+
+
 
 
     }

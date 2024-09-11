b1b8ef848c1bf66294c75239fb6c899513f9c85b
Jung-Eunmi <dmssl2486@gmail.com>
2024-09-11 12:32:09 +0900

    [240911]수수수정

--- chap08-inheritance-leture-source/src/main/java/com/ohgiraffers/section02/suprtKeyword/Application.java
+++ chap08-inheritance-leture-source/src/main/java/com/ohgiraffers/section02/suprtKeyword/Application.java
@@ -1,5 +1,7 @@
 package com.ohgiraffers.section02.suprtKeyword;
 
+import java.time.LocalDateTime;
+
 public class Application {
 
     public static void main(String[] args) {
@@ -13,6 +15,26 @@ public class Application {
         *   super() : 부모 생성자를 호출하는 구문으로 인자와 매개변수의
         *             타입, 갯수, 순서가 일치하는 부모의 생성자를 호출한다.
         * */
+
+        Product product1 = new Product();
+        System.out.println(product1.getInfo());
+
+        Product product2 = new Product("S-1234","삼송","갠역시폴드6",2220000, LocalDateTime.now());
+        System.out.println(product2.getInfo());
+
+        System.out.println("==============================================================");
+
+        Computer computer1 = new Computer();
+        System.out.println(computer1.getInfo());
+
+        Computer computer2 = new Computer("인텔",512,16,"윈도우");
+        System.out.println(computer2.getInfo());
+
+        Computer computer3 = new Computer("S-1234","삼송","갠역시폴드6",2220000, LocalDateTime.now(),
+                                          "인텔",512,16,"윈도우");
+        System.out.println(computer3.getInfo());
+
+
 
 
     }
--- chap08-inheritance-leture-source/src/main/java/com/ohgiraffers/section02/suprtKeyword/Computer.java
+++ chap08-inheritance-leture-source/src/main/java/com/ohgiraffers/section02/suprtKeyword/Computer.java
@@ -23,6 +23,56 @@ public class Computer extends Product {
         this.hdd = hdd;
         this.ram = ram;
         this.os = os;
-        System.out.println("Computer 클래스의 부모 필드도 같이 초기화 하는 생성자 호출함...");
+        System.out.println("Computer 클래스의 부모 필드도 같이 초기화 하는 생성자 호출됨...");
     }
+
+    public Computer(String cpu, int hdd, int ram, String os) {
+        this.cpu = cpu;
+        this.hdd = hdd;
+        this.ram = ram;
+        this.os = os;
+        System.out.println("Computer 클래스의 모든 필드를 초기화 하는 생성자 호출됨...");
+    }
+
+    public String getCpu() {
+        return cpu;
+    }
+
+    public void setCpu(String cpu) {
+        this.cpu = cpu;
+    }
+
+    public int getHdd() {
+        return hdd;
+    }
+
+    public void setHdd(int hdd) {
+        this.hdd = hdd;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
+
+    public String getOs() {
+        return os;
+    }
+
+    public void setOs(String os) {
+        this.os = os;
+    }
+
+    @Override
+    public String getInfo() {
+        return super.getInfo() + "Computer ["
+                + "cpu=" + this.cpu
+                + ", hdd=" + this.hdd
+                + ", ram=" + this.ram
+                + ", os=" + this.os + "]";
+    }
+
 }

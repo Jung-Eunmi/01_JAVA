3165abce70703745318900487a7125afb5ce0862
Jung-Eunmi <dmssl2486@gmail.com>
2024-09-12 17:43:17 +0900

    [240912] 최종

--- chap09-polymorphism-lecture-source/src/main/java/com/ohgiraffers/section01/Test/Car.java
+++ chap09-polymorphism-lecture-source/src/main/java/com/ohgiraffers/section01/Test/Car.java
@@ -1,54 +1,67 @@
-//package com.ohgiraffers.section01.Test;
-//
-//public class Car {
-//
-//    private boolean onOff;
-//    private int speed;
-//
-//    public void carOn() {
-//        if(onOff){
-//            System.out.println("이미 시동이 걸려있습니다.");
-//        }else {
-//            onOff = true;
-//            System.out.println("시동켜졌습니다.");
-//        }
-//    }
-//
-//    public void speedUp() {
-//        if(onOff){
-//            if(speed==0){
-//                speed += 10;
-//                System.out.println("엑셀을 밟았습니다."+speed+"km/h 시속으로 달리고 있습니다.");
-//
-//
-//        }else{
-//                System.out.println("시동을 켜주세요");
-//        }
-//    }
-//
-//    public void speedZero() {
-//        if(onOff){
-//
-//        }else {
-//            System.out.println("시동을 켜주세요. 브레이크를 밟아도 소용없습니다.");
-//        }
-//
-//    }
-//
-//    public void carOff() {
-//        if(onOff){
-//            if(speed){
-//
-//
-//            }else {
-//
-//            }
-//
-//
-//        }else {
-//            System.out.println("시동이 이미 꺼져있습니다.");
-//        }
-//    }
-//
-//
-//}
+package com.ohgiraffers.section01.Test;
+
+public class Car {
+
+    private boolean onOff;
+    private int speed;
+
+    public void carOn() {
+
+        if (onOff) {
+            System.out.println("이미 시동이 걸려있습니다.");
+        } else {
+            onOff = true;
+            System.out.println("시동켜졌습니다.");
+        }
+
+    }
+
+    public void speedUp() {
+        if (onOff) {
+
+            if (speed >= 0) {
+                speed += 10;
+                System.out.println("엑셀을 밟았습니다." + speed + "km/h 시속으로 달리고 있습니다.");
+            }
+
+        } else {
+            System.out.println("시동을 켜주세요");
+        }
+
+    }
+
+    public void speedZero () {
+
+        if (onOff) {
+
+            if (speed > 0) {
+                speed = 0;
+                System.out.println("브레이크를 밟았습니다.");
+            }
+
+        } else {
+            System.out.println("시동을 켜주세요. 브레이크를 밟아도 소용없습니다.");
+        }
+
+    }
+
+
+    public void carOff () {
+        if (onOff) {
+
+            if (speed == 0) {
+                onOff = false;
+                System.out.println("시동을 껐습니다");
+            } else {
+                System.out.println("속력이 있어 시동을 끌수 없습니다.");
+            }
+
+        } else {
+            System.out.println("시동이 이미 꺼져있습니다.");
+        }
+
+    }
+
+}
+
+
--- chap09-polymorphism-lecture-source/src/main/java/com/ohgiraffers/section01/Test/Caracer.java
+++ chap09-polymorphism-lecture-source/src/main/java/com/ohgiraffers/section01/Test/Caracer.java
@@ -1,22 +1,22 @@
-//package com.ohgiraffers.section01.Test;
-//
-//public class Caracer {
-//
-//    Car car =new Car();
-//
-//    public void on() {
-//        car.carOn();
-//    }
-//
-//    public void accelator() {
-//        car.speedUp();
-//    }
-//
-//    public void breakor() {
-//        car.speedZero();
-//    }
-//
-//    public void off() {
-//        car.carOff();
-//    }
-//}
+package com.ohgiraffers.section01.Test;
+
+public class Caracer {
+
+    private Car car =new Car();
+
+    public void on() {
+        car.carOn();
+    }
+
+    public void accelator() {
+        car.speedUp();
+    }
+
+    public void breakor() {
+        car.speedZero();
+    }
+
+    public void off() {
+        car.carOff();
+    }
+}
